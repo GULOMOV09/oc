88b78625e9c4d363ce209b423b7f148973b6dd22
fazliddin <bovaqulov@gmail.com>
2024-04-03 10:54:01 +0500

    dfsdfvdsf

---
+++ script.js
@@ -0,0 +1,15 @@
+var matn = prompt('Matn kiriting:');
+var num = num(matn);
+console.log('Matnda', num, 'ta harf unli.')
+function num(unlilar) {
+    var num = 0;
+    var unlilar = ['a', 'e', 'i', 'o', 'u'];
+    for (var i = 0; i < unlilar.length; i++) {
+      var harf = matn[i].toLowerCase();
+      
+      if (unlilar.includes(harf)) {
+        num++;
+      }
+    }
+    return num;
+  }
